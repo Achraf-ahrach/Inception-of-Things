c9c8fe420093c399a0ec7dd0fcabf9b0c2b11406
Achraf Ahrach <achrafahrach44@gmail.com>
2025-05-14 20:20:43 +0100

    feat: part 1

---
+++ p1/scripts/server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Update and install required packages
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y curl
+
+echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+# Install K3s server
+curl -sfL https://get.k3s.io | sh -
+
+# Wait for a few seconds to ensure k3s is running
+sleep 10
+
+# Allow the vagrant user to run kubectl without sudo
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
+
+# Set KUBECONFIG environment variable in .bashrc
+echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+
+# Get the token and save it to a file that can be shared with agents
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+
+# Setup kubeconfig
+# mkdir -p ~/.kube
+# sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+# sudo chown $USER:$USER ~/.kube/config
