080177485043087ab2b4a2097029dd36c2e2e18d
Achraf Ahrach <achrafahrach44@gmail.com>
2025-05-30 15:57:17 +0100

    feat: p2 runnig, but  http://192.168.56.110 not work

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,21 +1,97 @@
 #!/bin/bash
 
-# Update and install required packages
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y curl
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
 
+print() {
+  echo -e "${BLUE}========================================${RESET}"
+  echo -e "${BLUE}$1${RESET}"
+  echo -e "${BLUE}========================================${RESET}"
+}
+
+# Function to handle errors
+handle_error() {
+  echo -e "${RED}Error: $1${RESET}"
+  exit 1
+}
+
+info() {
+  echo -e "${YELLOW}Info: $1${RESET}"
+}
+
+# Update and install required packages
+# sudo apt-get update && sudo apt-get upgrade -y
+print "Checking if curl is installed"
+command -v curl
+if [ $? -eq 0 ]; then
+  info "curl is already installed"
+else
+  sudo apt install curl -y || handle_error "Failed to install curl"
+fi
+
+print "Installing K3s"
+curl -sfL https://get.k3s.io | sh -s - --flannel-iface eth1 || handle_error "Failed to install k3s"
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
+
+# Add alias for kubectl
+print "Adding alias for kubectl"
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
 
-# Install K3s server with static IP
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
-
 # Wait for a few seconds to ensure k3s is running
-sleep 10
+sleep 10s
 
-# Allow the vagrant user to run kubectl without sudo
+# Setup kubeconfig for vagrant user
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube
-
-# Set KUBECONFIG environment variable in .bashrc
 echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+
+# Apply deployment manifests
+print "Applying deployment manifests"
+cd /tmp/deployment || handle_error "Directory /tmp/deployment not found"
+sudo kubectl apply -f . || handle_error "Failed to apply deployment manifests"
+
+# Wait for all pods to be in running state
+# print "Waiting for all pods to be in running state"
+# sleep 10s ; sudo kubectl wait --for=condition=Ready pods --all --timeout=300s || handle_error "Not all pods are in running state"
+sleep 10s
+
+# if [ $? -eq 0 ]; then
+#   info "All pods are in running state"
+# else
+#   handle_error "Not all pods are in running state"
+# fi
+
+# Apply service manifests
+print "Applying service manifests"
+cd /tmp/services || handle_error "Directory /tmp/services not found"
+sudo kubectl apply -f . || handle_error "Failed to apply service manifests"
+
+# Wait for all services to be in running state
+print "Waiting for all services to be in running state"
+sudo  kubectl wait --for=jsonpath='{.spec.selector.app}' -f /tmp/services/ --timeout=300s || handle_error "Not all services are in running state"
+
+
+print "Applying ingress manifests"
+cd /tmp/ingress || handle_error "Directory /tmp/ingress not found"
+sudo kubectl apply -f . || handle_error "Failed to apply ingress manifests"
+
+print "Script Complete"
+echo -e "${GREEN}All manifests have been successfully applied${RESET}"
+echo -e "${GREEN} You can Access The website by visiting ${YELLOW}http://192.168.56.110${RESET}"
+echo -e "${GREEN} The Port IS BELOW "
+# var=$(sudo kubectl get svc -n kube-system --kubeconfig /etc/rancher/k3s/k3s.yaml)
+# echo -e "${GREEN} $var ${RESET}"
+
+
+# //////////////////
+
+# Apply Kubernetes manifests
+print "Applying Kubernetes manifests..."
+# sudo -u vagrant kubectl apply -f /tmp/deployment/apps.yaml
+# sudo -u vagrant kubectl apply -f /tmp/services/apps-service.yaml
+# sudo -u vagrant kubectl apply -f /tmp/ingress/ingress-config.yaml
+
