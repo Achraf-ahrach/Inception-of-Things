e15dbf604a8113c189845250617b5b9a4a4a06e4
Achraf-ahrach <achrafahrach44@gmail.com>
2025-06-17 13:03:42 +0100

    feat: p2 is working now

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,82 +1,40 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-RED='\033[0;31m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
 
-# Functions
+# Function
 print() {
   echo -e "${BLUE}========================================${RESET}"
   echo -e "${BLUE}$1${RESET}"
   echo -e "${BLUE}========================================${RESET}"
 }
 
-handle_error() {
-  echo -e "${RED}Error: $1${RESET}"
-  exit 1
-}
-
-info() {
-  echo -e "${YELLOW}Info: $1${RESET}"
-}
-
-
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y curl
-print "system updated and curl installed"
-
 print "Installing K3s"
-curl -sfL https://get.k3s.io | sh -s - --flannel-iface eth1 || handle_error "Failed to install k3s"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip="192.168.56.110" --write-kubeconfig-mode 644" sh -
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 # Wait for a few seconds to ensure k3s is running
-sleep 10s
+sleep 20
 
-# Add alias for kubectl
-print "Adding alias for kubectl"
-echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+# Deploy Nginx Ingress Controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/cloud/deploy.yaml
 
-# Setup kubeconfig for vagrant user
-sudo mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube
-echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+# Step 1 — Wait for controller to be ready
+kubectl wait --namespace ingress-nginx --for=condition=Available deployment/ingress-nginx-controller --timeout=180s
 
-# Apply deployment manifests
-print "Applying deployment manifests"
-sudo kubectl apply -f /tmp/deployment/apps.yaml || handle_error "Failed to apply deployment manifests"
-print "Deployment manifests applied successfully"
+print "Waiting for the admission webhook job to finish..."
+kubectl wait --for=condition=complete -n ingress-nginx job/ingress-nginx-admission-create --timeout=180s
 
-# # Wait for all pods to be in running state
-# print "Waiting for all pods to be in running state"
-# sleep 10s ; sudo kubectl wait --for=condition=Ready pods --all --timeout=300s || handle_error "Not all pods are in running state"
-# # sleep 10s
+print "Waiting for admission webhook endpoint to become available..."
+timeout 180 bash -c '
+  until kubectl get endpoints ingress-nginx-controller-admission -n ingress-nginx | grep -q ":8443"; do
+    echo "... still waiting ..."
+    sleep 5
+  done
+'
 
-# # if [ $? -eq 0 ]; then
-# #   info "All pods are in running state"
-# # else
-# #   handle_error "Not all pods are in running state"
-# # fi
-
-# # Apply service manifests
-print "Applying service manifests"
-sudo kubectl apply -f /tmp/services/apps-service.yaml || handle_error "Failed to apply service manifests"
-print "Service manifests applied successfully"
-
-# # Wait for all services to be in running state
-# print "Waiting for all services to be in running state"
-# sudo  kubectl wait --for=jsonpath='{.spec.selector.app}' -f /tmp/services/ --timeout=300s || handle_error "Not all services are in running state"
-# # kubectl wait --for=condition=available --timeout=60s deployment/app1
-
-
-print "Applying ingress manifests"
-sudo kubectl apply -f /tmp/ingress/ingress-config.yaml || handle_error "Failed to apply ingress manifests"
-print "Ingress manifests applied successfully"
-
-print "Script Complete"
-echo -e "${GREEN}All manifests have been successfully applied${RESET}"
-echo -e "${GREEN} You can Access The website by visiting ${YELLOW}http://192.168.56.110${RESET}"
-echo -e "${GREEN} The Port IS BELOW "
-var=$(sudo kubectl get svc -n kube-system --kubeconfig /etc/rancher/k3s/k3s.yaml)
-echo -e "${GREEN} $var ${RESET}"
+print "Applying deployment apps"
+kubectl apply -f /vagrant/apps/
+print "Deployment apps applied successfully"
